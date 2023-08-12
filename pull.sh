f160967c017bca5b9fd359736cec46abd0469a73
Stéphane Bressani <stephane@bressani.dev>
2023-08-12 14:21:04 +0200

    intégration

--- pull.sh
+++ pull.sh
@@ -21,8 +21,8 @@ else
         sudo rm -rf /var/www/astro.fcgi/build
         sudo cp -r /home/ubuntu/www/astro.fcgi/build /var/www/astro.fcgi
 
-        sudo chmod -R 775 bin
-        sudo chown -R www-data:www-data bin
+        sudo chmod -R 775 build
+        sudo chown -R www-data:www-data build
         sudo chmod +x /var/www/astro.fcgi/build/astro.fcgi
     "
 
