b4ab92c3e23e7a9003dde731f4a245e6db271b0f
Stéphane Bressani <stephane@bressani.dev>
2023-08-12 14:16:25 +0200

    intégration

--- pull.sh
+++ pull.sh
@@ -2,6 +2,6 @@
 if [ -n "$(git status --porcelain)" ]; then
   echo "GIT: there are changes";
 else
-  ssh ubuntu@astrologie-traditionnelle.net "sudo rm -rf /home/ubuntu/www/astro.fcgi && cd /home/ubuntu/www && git clone git@github.com:stephaneworkspace/astro.fcgi.git && cd /home/ubuntu/www/astro.fcgi && rm -rf /home/ubuntu/www/astro.fcgi/build && mkdir /home/ubuntu/www/astro.fcgi/build && cd /home/ubuntu/www/astro.fcgi/build && cmake .. && make && git pull && sudo rm -rf /var/www/astro.fcgi/build && sudo cp -r /home/ubuntu/www/astro.fcgi/build /var/www/astro.fcgi && sudo chown -R 775 bin && sudo chown -R www-data:www-data bin && sudo chmod +x /var/www/astro.fcgi/build/astro.fcgi"
+  ssh ubuntu@astrologie-traditionnelle.net "sudo rm -rf /home/ubuntu/www/astro.fcgi && cd /home/ubuntu/www && git clone git@github.com:stephaneworkspace/astro.fcgi.git && cd /home/ubuntu/www/astro.fcgi && rm -rf /home/ubuntu/www/astro.fcgi/build && mkdir /home/ubuntu/www/astro.fcgi/build && cd /home/ubuntu/www/astro.fcgi/build && cmake -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ .. && make && git pull && sudo rm -rf /var/www/astro.fcgi/build && sudo cp -r /home/ubuntu/www/astro.fcgi/build /var/www/astro.fcgi && sudo chown -R 775 bin && sudo chown -R www-data:www-data bin && sudo chmod +x /var/www/astro.fcgi/build/astro.fcgi"
   echo "done"
 fi
