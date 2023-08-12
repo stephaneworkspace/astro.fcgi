b962b5faab5e95312aabdf41dfb844f7e5613bd1
Stéphane Bressani <stephane@bressani.dev>
2023-08-12 17:25:51 +0200

    changement pour tester

--- Sources/astro.cpp
+++ Sources/astro.cpp
@@ -21,50 +21,46 @@ using namespace std;
     FCGX_InitRequest(&request, 0, 0);
 
 #if SW_DEBUG
-    string simulatedQueryString = "lat=" + string(argv[1]) + "&lng=" + string(argv[2]);
-    map<string, string> params = Util::parseQueryString(simulatedQueryString);
-    string lat = params["lat"].c_str();
-    string lng = params["lng"].c_str();
-#else
-    std::cerr << "Information de log ici 0." << endl;
-    const char* queryString = FCGX_GetParam("QUERY_STRING", request.envp);
-    std::cerr << "Information de log ici 00." << endl;
-    string qS = queryString ? string(queryString) : "";
-    std::cerr << "Information de log ici 000." << endl;
-    string lat;
-    string lng;
-    std::cerr << "Information de log ici 1." << endl;
-    if (queryString) {
-        map<string, string> params = Util::parseQueryString(qS);
-        string lat = params["lat"];
-        string lng = params["lng"];
-    }
-#endif
-
-    double f_lat = 0.0;
-    try {
-        f_lat = stof(lat);
-    } catch (const invalid_argument& ia) {
-        f_lat = 0.0;
-    } catch (const out_of_range& oor) {
-        f_lat = 0.0;
-    }
-    double f_lng = 0.0;
-    try {
-        f_lng = stof(lng);
-    } catch (const invalid_argument& ia) {
-        f_lng = 0.0;
-    } catch (const out_of_range& oor) {
-        f_lng = 0.0;
-    }
-
-    std::cerr << "Information de log ici 2." << endl;
-
-#if SW_DEBUG
     while (FCGI_Accept() >= 0) {
+        string simulatedQueryString = "lat=" + string(argv[1]) + "&lng=" + string(argv[2]);
+        map<string, string> params = Util::parseQueryString(simulatedQueryString);
+        string lat = params["lat"].c_str();
+        string lng = params["lng"].c_str();
 #else
     while (FCGX_Accept_r(&request) == 0) {
+        std::cerr << "Information de log ici 0." << endl;
+        const char* queryString = FCGX_GetParam("QUERY_STRING", request.envp);
+        std::cerr << "Information de log ici 00." << endl;
+        string qS = queryString ? string(queryString) : "";
+        std::cerr << "Information de log ici 000." << endl;
+        string lat;
+        string lng;
+        std::cerr << "Information de log ici 1." << endl;
+        if (queryString) {
+            map<string, string> params = Util::parseQueryString(qS);
+            string lat = params["lat"];
+            string lng = params["lng"];
+        }
 #endif
+        double f_lat = 0.0;
+        try {
+            f_lat = stof(lat);
+        } catch (const invalid_argument& ia) {
+            f_lat = 0.0;
+        } catch (const out_of_range& oor) {
+            f_lat = 0.0;
+        }
+        double f_lng = 0.0;
+        try {
+            f_lng = stof(lng);
+        } catch (const invalid_argument& ia) {
+            f_lng = 0.0;
+        } catch (const out_of_range& oor) {
+            f_lng = 0.0;
+        }
+
+        std::cerr << "Information de log ici 2." << endl;
+
         std::cerr << "Information de log ici 3." << endl;
         Swe sweInstance(2000, 5, 15, 12, 30, f_lat, f_lng, 1);
 
