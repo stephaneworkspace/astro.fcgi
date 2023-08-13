f65fb37e3542d65732ddd23ba78d6d514a192b01
Stéphane Bressani <stephane@bressani.dev>
2023-08-13 12:19:02 +0200

    ajout -g pour debug

--- pull.sh
+++ pull.sh
@@ -2,33 +2,35 @@
 if [ -n "$(git status --porcelain)" ]; then
   echo "GIT: there are changes";
 else
-    ssh ubuntu@astrologie-traditionnelle.net "
-        sudo rm -rf /home/ubuntu/www/astro.fcgi
-        cd /home/ubuntu/www
+  DEBUG="-DCMAKE_BUILD_TYPE=Debug"
+  #DEBUG=""
+  ssh ubuntu@astrologie-traditionnelle.net "
+      sudo rm -rf /home/ubuntu/www/astro.fcgi
+      cd /home/ubuntu/www
 
-        git clone git@github.com:stephaneworkspace/astro.fcgi.git
-        cd /home/ubuntu/www/astro.fcgi
+      git clone git@github.com:stephaneworkspace/astro.fcgi.git
+      cd /home/ubuntu/www/astro.fcgi
 
-        git pull
+      git pull
 
-        rm -rf /home/ubuntu/www/astro.fcgi/build
-        mkdir /home/ubuntu/www/astro.fcgi/build
+      rm -rf /home/ubuntu/www/astro.fcgi/build
+      mkdir /home/ubuntu/www/astro.fcgi/build
 
-        cd /home/ubuntu/www/astro.fcgi/build
-        cp /home/ubuntu/ephem/*.se1 /home/ubuntu/www/astro.fcgi/build/
+      cd /home/ubuntu/www/astro.fcgi/build
+      cp /home/ubuntu/ephem/*.se1 /home/ubuntu/www/astro.fcgi/build/
 
-        export C=/usr/bin/clang
-        export CXX=/usr/bin/clang++
-        cmake -DSW_DEBUG=0 ..
-        make astro.fcgi
-        cp -r /home/ubuntu/www/astro.fcgi/build/bin/astro.fcgi /home/ubuntu/www/astro.fcgi/build/astro.fcgi
-        sudo rm -rf /var/www/astro.fcgi/build
-        sudo cp -r /home/ubuntu/www/astro.fcgi/build /var/www/astro.fcgi
+      export C=/usr/bin/clang
+      export CXX=/usr/bin/clang++
+      cmake -DSW_DEBUG=0 $DEBUG..
+      make astro.fcgi
+      cp -r /home/ubuntu/www/astro.fcgi/build/bin/astro.fcgi /home/ubuntu/www/astro.fcgi/build/astro.fcgi
+      sudo rm -rf /var/www/astro.fcgi/build
+      sudo cp -r /home/ubuntu/www/astro.fcgi/build /var/www/astro.fcgi
 
-        sudo chmod -R 775 /var/www/astro.fcgi/build
-        sudo chown -R www-data:www-data /var/www/astro.fcgi/build
-        sudo chmod +x /var/www/astro.fcgi/build/astro.fcgi
-    "
+      sudo chmod -R 775 /var/www/astro.fcgi/build
+      sudo chown -R www-data:www-data /var/www/astro.fcgi/build
+      sudo chmod +x /var/www/astro.fcgi/build/astro.fcgi
+  "
 
-    echo "done"
+  echo "done"
 fi
